9259c931a3a29075ad2c5b9b981a5b0e694a0539
Jesse <bpara001@ucr.edu>
2020-04-11 17:09:10 -0700

    -Json and video info gathering

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import nltk
 import random
+import json
+import re
+import urllib.request
+from pytube import YouTube
 
+api_key="AIzaSyDugJ3NMj9r6GOes-_NBkTKzyILLpkrr4A"
+video_id=""
+url= f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}"
+
+json_url =urllib.request.urlopen(url)
+data=json.loads(json_url.read())
+print(data)
